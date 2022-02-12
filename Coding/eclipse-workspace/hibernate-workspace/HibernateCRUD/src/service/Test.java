9e93201660b7802e96fda07a6a1dfc47f6bc70b8
Anand V V <anand.vijay@gmail.com>
2022-02-11 23:41:14 -0500

    Hibernate CRUD Demo

---
+++ Coding/eclipse-workspace/hibernate-workspace/HibernateCRUD/src/service/Test.java
@@ -0,0 +1,45 @@
+package service;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import entity.Teacher;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Connecting to database");
+		SessionFactory factory = null;
+		Session session = null;
+		
+		factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.buildSessionFactory();
+		
+		System.out.println("created");
+		
+		Teacher t1 = new Teacher("Shalini", "Garg", "shalinig@gl.com");
+		Teacher t2 = new Teacher("Sheetu", "Kapoor", "sheetu@gl.com");
+		Teacher t3 = new Teacher("Ria", "Dhopte", "ria@gl.com");
+		Teacher t4 = new Teacher("Jenny", "Joe", "jenny@gl.com");
+		
+		Transaction tx = null;
+		
+		try {
+			session = factory.getCurrentSession();
+			tx = session.beginTransaction();
+			session.save(t1);
+			session.save(t2);
+			session.save(t3);
+			session.save(t4);
+			tx.commit();
+		}catch(Exception e) {
+			System.out.println("Error");
+			e.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}
+	}
+}
