6d7fba8d07d12edae191c8d5de0a15e617f6156c
Anand V V <anand.vijay@gmail.com>
2022-06-18 16:20:26 -0400

    typescript

---
+++ Coding/typescript/01-types.js
@@ -0,0 +1,6 @@
+var isDone = true;
+var num = 10;
+var firstName = "anand";
+console.log(isDone);
+console.log(num);
+console.log(firstName);
---
+++ Coding/typescript/01-types.ts
@@ -0,0 +1,13 @@
+var isDone: boolean = true;
+var num: number = 10;
+var firstName: string = "anand";
+
+console.log(isDone);
+console.log(num);
+console.log(firstName);
+
+
+
+
+
+
