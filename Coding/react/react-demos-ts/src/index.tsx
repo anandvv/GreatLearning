4da798a5405d9f52c4d3008c172df812568145a2
Anand V V <anand.vijay@gmail.com>
2022-06-18 16:55:34 -0400

    Basic React App using TypeScript
    npx create-react-app <appname> --template=typescript

---
+++ Coding/react/react-demos-ts/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+const el = (
+    <div title="Welcome to React">
+        <h1>An introduction to React!</h1>
+        <p id="introduction">
+            <span className="text-bold">React</span>
+            &nbsp;is a JS library for building the UI
+        </p>
+    </div>
+);
+
+ReactDom.render(el, document.getElementById('root'));
