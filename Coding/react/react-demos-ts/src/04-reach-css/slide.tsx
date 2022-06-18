9570e37289289e08cab2d2cac90bde540f8efdb1
Anand V V <anand.vijay@gmail.com>
2022-06-18 17:51:20 -0400

    React CSS Styling

--- Coding/react/react-demos-ts/src/04-reach-css/slide.tsx
+++ Coding/react/react-demos-ts/src/04-reach-css/slide.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './slide.css';
 
-const headingStyle = {
-    fontSize: '4em'
-}
-
-const bodyStyle = {
-    fontSize: '2.5em'
-}
+import { 
+    bodyStyle,
+    listUnstyled,
+    headingFinalStyle
+} from './slides.styles'; 
 
 const el = (
     <div className="slide">
-        <h1 style={headingStyle}>Styling in React</h1>
+        <h1 style={headingFinalStyle}>Styling in React</h1>
         <hr />
-        <ul style={bodyStyle}>
+        <ul style={ {...bodyStyle, ...listUnstyled} }>
             <li>You can style using CSS classes</li>
             <li>You can style using inline styling</li>
         </ul>
