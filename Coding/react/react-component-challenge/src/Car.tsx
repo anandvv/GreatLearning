515af43a3027f9da6ab531d99e6a3ee761dfae76
Anand V V <anand.vijay@gmail.com>
2022-06-18 21:49:09 -0400

    Update Car.tsx

--- Coding/react/react-component-challenge/src/Car.tsx
+++ Coding/react/react-component-challenge/src/Car.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 class Car extends React.Component {
     render() {
-      const car = (
-        <><h2>Hi, I am a Car!</h2><p>lorem ipsum dolor sit amet</p></>
+      return (
+        <>
+          <h2>Hi, I am a Car!</h2>
+          <p>lorem ipsum dolor sit amet</p>
+        </>
       );
-      return car;
     }
 }
 
