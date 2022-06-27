752050e952ac6f66237e27ac4c02632988d2abfe
Anand V V <anand.vijay@gmail.com>
2022-06-27 15:12:41 -0400

    React Refs

---
+++ Learning Material/09_React/react-refs/src/ChildComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class ComponentA extends React.Component {
+
+    render() {
+
+        return(
+
+<div> 
+
+<h1>This is componentA </h1>
+
+           </div>
+
+        );
+
+    }
+
+}
---
+++ Learning Material/09_React/react-refs/src/ParentComponent.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ComponentA from './ChildComponent';
+
+class ComponentB extends React.Component {
+
+    render() {
+
+        const style1 = { backgroundColor : black, color : white } <h1 style={style1}></h1>
+        const style2 = { backgroundColor : black, color : white } 
+        <h1 style={{...style1, height : 300}}>
+        const style1 = { backgroundColor : black, color : white } <h1 style={{...style1}}>
+
+        return (
+
+            <div>
+
+                <ComponentA />
+                { 4>5 ? <> hello </> : <> goodbye </> }
+                { 4>5 && <> ….</>}
+                
+
+            </div>
+
+        ); 
+
+    }
+
+}
